7d43ae79eae222c22f48edc4d04784d3bb877cf1
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-07-31 03:12:31 -0500

    Add title, description, and location to events and association between user and event

---
+++ db/migrate/20250731080521_add_title_description_and_location_to_events.rb
@@ -0,0 +1,7 @@
+class AddTitleDescriptionAndLocationToEvents < ActiveRecord::Migration[8.0]
+  def change
+    add_column :events, :title, :string
+    add_column :events, :description, :text
+    add_column :events, :location, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_31_061349) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_31_080521) do
   create_table "events", force: :cascade do |t|
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "description"
+    t.string "location"
   end
 
   create_table "users", force: :cascade do |t|
