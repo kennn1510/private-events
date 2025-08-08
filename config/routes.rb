225bcf52a6d39948ca950759e0dc74dca5d87621
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-08-08 11:51:16 +0700

    Remove extraneous user/show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get "users/show"
-  get "user/show"
   devise_for :users
   get "events/index"
+  get "users/show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
